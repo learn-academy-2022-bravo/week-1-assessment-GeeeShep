ac3c3f891c8db0439c447fe7913bb9f3970e3643
GeeeShep <gashep@yahoo.com>
2022-03-27 22:32:42 -0700

    completed assesement

--- code-challenges.js
+++ code-challenges.js
@@ -7,43 +7,224 @@
 // --------------------INSTRUCTOR EXAMPLE: Create a function that takes in any two strings and returns the string with more characters. Use the two sets of test variables provided.
 
 // Set one:
-const fruit1 = "apple"
-const fruit2 = "banana"
+//const fruit1 = "apple"
+//const fruit2 = "banana"
 
 // Set two:
-const fruit3 = "cherry"
-const fruit4 = "kiwi"
-
+//const fruit3 = "cherry"
+//const fruit4 = "kiwi"
 
 // --------------------1) Create a function that takes in any given number and determines if the number is below boiling point, at boiling point, or above boiling point. Boiling point is 212 degrees Fahrenheit. Use the test variables provided below. Expected output: "42 is below boiling point", "350 is above boiling point", "212 is at boiling point"
 
-const temp1 = 42
-const temp2 = 350
-const temp3 = 212
+// function boiling point
+// inputs are the numbers 42, 350, 212
+// output will be a string saying "42 is below boiling point", 350 is above boiling point", "212 is at boiling point"
+
+//create a function called tempRead that gives different results for the temperature. 
+//if the temp says 42 then "below boiling point"
+//if temp says 350 then "at boiling point" 
+//if temp says 212 then "is at boiling point"
+
+//(forgot to save what i had originally)
+//i was originally thinking that i needed to do an array where instead of nums it would be the three constants. Maybe over thinking??
+
+//const temp1 = 42
+//const temp2 = 350
+//const temp3 = 212
+
+//const tempRead = (nums) => {
+//if(nums < 212) {
+ //   return `${nums} is below boiling point`
+//} 
+//else if (nums > 212) {
+//    return `${nums} is above boiling point`
+//} 
+//else if (nums = 212) {
+//    return  `${nums} is at boiling point`
+//  }
+//}
+//console.log(tempRead(temp1))
+//console.log(tempRead(temp2))
+//console.log(tempRead(temp3))
+//console.log(tempRead("nums is below boiling point", "nums is above boiling point", "nums is at boiling point"))
+
+//// learnacademy@LEARNs-Air-4 week-1-assessment-GeeeShep % node code-challenges.js
+// nums is at boiling point
+//// learnacademy@LEARNs-Air-4 week-1-assessment-GeeeShep % node code-challenges.js
+// 212 nums is at boiling point
+//// learnacademy@LEARNs-Air-4 week-1-assessment-GeeeShep % node code-challenges.js
+// 42 nums is below boiling point
+
+// above this is what i originally had but it gave me an output of "nums is below point", etc...
+//// `${nums} is below boiling point` this is how the codes captures an input dynamically, didnt have this originally.
+
+//// learnacademy@LEARNs-Air-4 week-1-assessment-GeeeShep % node code-challenges.js
+// 42 is below boiling point
+// 350 is above boiling point
+// 212 is at boiling point
 
 
 // --------------------2) Create the code that will combine the two arrays and return the length using the variables provided below. Expected output: 10
 
-const myNumbers1 = [3, 7, 0, 36, -9]
-const myNumbers2 = [8, -7, 42, 9, 13]
+//inputs are myNumbers1 and myNumbers2
+//1st output...joining the array
+//2nd output ... length of 10
+
+//process
+//use an accessor .concat to join array
+//use .length property to get total output of array
+
+//const myNumbers1 = [3, 7, 0, 36, -9]
+//const myNumbers2 = [8, -7, 42, 9, 13]
+
+//console.log(myNumbers1.concat(myNumbers2))
+//ran this just to fact check to make sure im on the right track
+//error...forgot to save..hehe
+
+//console.log(myNumbers1.concat(myNumbers2).length)
+//ran this originally and it worked
+
+//const joinNums = myNumbers1.concat(myNumbers2)
+
+//console.log(joinNums.length)
+//tried this other way and it worked too
+////[
+    // learnacademy@LEARNs-Air-4 week-1-assessment-GeeeShep % node code-challenges.js
+//[
+//   3,  7,  0, 36, -9,
+//   8, -7, 42,  9, 13
+// ]
 
 
 // --------------------3) Create the code that will reverse the letters of a string using the test variable provided below. Expected output: "1202 ovarB"
 
-const currentCohort = "Bravo 2022"
+// need to use a built-in-method, .reverse
+//use another built-in-method, .join
+//1. Change a string to an array (you must do this because the reverse ONLY works on an array).  2. Then reverse it.  3. Then “join” it back together.
+
+// const currentCohort = "Bravo 2022"
+// let nameChars = Array.from(currentCohort)
+// console.log(nameChars)
+
+// console.log(nameChars.reverse()) 
+// console.log(nameChars.join(''))
+
+
+// let nameChar = Array.from(currentCohort)
+// console.log(nameChar)
+
+// currentCohort = (currentCohort.reverse().join)
+
+// console.log(currentCohort)
+
+// console.log(currentCohort)
+
+// console.log(currentCohort.reverse("Bravo 2022"))
+
+
+// learnacademy@LEARNs-Air-4 week-1-assessment-GeeeShep % node code-challenges.js
+// [
+//   'B', 'r', 'a', 'v',
+//   'o', ' ', '2', '0',
+//   '2', '2'
+// ]
+// [
+//   '2', '2', '0', '2',
+//   ' ', 'o', 'v', 'a',
+//   'r', 'B'
+// ]
+// 2202 ovarB
+//----------------------------
+
 
 
 // --------------------4) Create a statement that will evaluate whether each number in an array is even or odd. Use the test variable provided below. Expected output: "odd" "even" "odd" "even" "odd" "even"
 
-const myArray = [13, 34, 5, 10, 27, 42]
+//tough one here. 
+//i believe i want todo an iteration using For loop.
+//the count starts at O
+//how many iterations i want the loop to do??? 6?
+//taking each number and dividing bt 2 to determine if its even or not
+//if it doesnt equal to 0 then its odd
+//else if it does then its even
 
+
+// const myArray = [13, 34, 5, 10, 27, 42]
+
+// for(let i = 0; i<myArray.length; i++){
+//  if(myArray[i] % 2 !==0){
+//    console.log(`"odd"`)
+//  } else{
+//      console.log(`"even"`)
+//  }
+// }
+// learnacademy@LEARNs-Air-4 week-1-assessment-GeeeShep % node code-challenges.js
+// "odd"
+// "even"
+// "odd"
+// "even"
+// "odd"
+// "even"
+
+//ALL THE PREVIOUS ATTEMPTS, I FORGOT TO WRITE DOWN MY PROCESSES.
+// 13 is an odd number
+// 5 is an odd number
+// 27 is an odd number
+// learnacademy@LEARNs-Air-4 week-1-assessment-GeeeShep % node code-challenges.js node code-challenges.js
+// 13 odd
+// 5 odd
+// 27 odd
+// learnacademy@LEARNs-Air-4 week-1-assessment-GeeeShep % node code-challenges.js node code-challenges.js
+// 13 odd
+// 34 even
+// 5 odd
+// 10 even
+// 27 odd
+// 42 even
+// learnacademy@LEARNs-Air-4 week-1-assessment-GeeeShep % node code-challenges.js node code-challenges.js
+// 13 "odd"
+// 34 "even"
+// 5 "odd"
+// 10 "even"
+// 27 "odd"
+// 42 "even"
+// learnacademy@LEARNs-Air-4 week-1-assessment-GeeeShep % node code-challenges.js node code-challenges.js
+// 13,34,5,10,27,42 "odd"
+// 13,34,5,10,27,42 "even"
+// 13,34,5,10,27,42 "odd"
+// 13,34,5,10,27,42 "even"
+// 13,34,5,10,27,42 "odd"
+// 13,34,5,10,27,42 "even"
+// learnacademy@LEARNs-Air-4 week-1-assessment-GeeeShep % node code-challenges.js node code-challenges.js
+// 13,34,5,10,27,42 "odd"
+// 13,34,5,10,27,42 "even"
+// 13,34,5,10,27,42 "odd"
+// 13,34,5,10,27,42 "even"
+// 13,34,5,10,27,42 "odd"
+// 13,34,5,10,27,42 "even"
+// learnacademy@LEARNs-Air-4 week-1-assessment-GeeeShep % node code-challenges.js node code-challenges.js
+// 13 "odd"
+// 34 "even"
+// 5 "odd"
+// 10 "even"
+// 27 "odd"
+// 42 "even"
+//-----------------------------
 
 // --------------------5) Create a function that takes in a set of any two numbers and subtracts the smaller number from the larger number using the sets of test variables provided below. Expected output: 42, 3
 
-// Set one:
-const number1 = 58
-const number2 = 100
+// so i know i need to get an output of 42 and 3 by subtracting the two constants in each set
+//i first tried console.log(number2 - number1) which gave me 42.
+//now i can put them together in a string to get the output on the same line
 
-// Set two:
-const number3 = 27
-const number4 = 24
+// // Set one:
+ const number1 = 58
+ const number2 = 100
+
+// // Set two:
+ const number3 = 27
+ const number4 = 24
+
+ //console.log(number2 - number1)
+
+//console.log(number2 - number1, (number3 - number4))
--- coding-concepts.js
+++ coding-concepts.js
@@ -7,53 +7,62 @@
 
 // --------------------INSTRUCTOR EXAMPLE: What will this log?
 const colors = ["tangerine", "magenta", "lilac", "daffodil"]
-// console.log(colors.push("indigo"))
+//console.log(colors.push("indigo"))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer:i believe this will put "indigo" at the end of the array
+// b) Verify and explain:Ok so it gave me an output of "5". After review i was partially correct but i didnt state(forgot) that it would give the value(length) of 5 since  the other elements are 1 thru 4.
+
 
 
 // --------------------1) What will this log?
 
 const cohort = "LEARN 2022"
-// console.log(cohort.length)
+//console.log(cohort.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer:This should give me an output of 10 
+// b) Verify and explain: becuase  its telling me the number of characters in the string. so this(length) is a  property that tells us how long a string is. Which starts from 1 and ends at 10
 
 
 // --------------------2) What will this log?
 
 const greeting = "Hello World!"
-// console.log(greeting[4])
+ //console.log(greeting[4])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer:this should give me an output of the first letter "o". 
+// b) Verify and explain: This is telling me that the console log is asking for 
+// a specific  character in the string which is indentified by the index of 4, starting from 0
 
 
 // --------------------3) What will this log?
 
 const languages = ["JavaScript", "Ruby", "Python", "C++"]
-const index = 1
-// console.log(languages[index])
+//const index = 1
+//console.log(languages[index])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: Ruby
+// b) Verify and explain: verified! within the languages array the string of words are indexed with "Ruby" being the indexed as 1, basically the value of Ruby is at the 1st index
 
 
 // --------------------4) What will this log?
 
 const weekendDays = ["saturday", "sunday"]
-// console.log(weekendDays.toUpperCase())
 
-// a) Your answer:
-// b) Verify and explain:
+//console.log(weekendDays.toUpperCase())
+
+
+// a) Your answer: I thought the answer would be SATURDAY, SUNDAY but i was incorrect
+// b) Verify and explain:the constant needs to be changed to a string then we can use the BIM toUpperCase to change the characters
+//console.log(weekendDays.toString().toUpperCase())
+
+
 
 
 // --------------------5) What will this log?
 
-const dataTypes = ["number", "string", "Boolean"]
-// console.log(typeof dataTypes.length)
+const dataTypes = ["hello", "string", "Boolean"]
+ console.log(typeof dataTypes.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: not sure
+// b) Verify and explain: so the output was "number". I changed "numbers" to "hello" for my own verification and it still gave me numbers.
+// .length identifies the character position of an element
+
